21929bfb39582304406788ea6caf797d6dc3ccca
renegade <renegadethousand@gmail.com>
2022-01-04 15:43:07 +0300

    Remove duplicates from sorted array

--- src/RemoveDuplicatesFromSortedArray.java
+++ src/RemoveDuplicatesFromSortedArray.java
@@ -4,8 +4,8 @@ public class RemoveDuplicatesFromSortedArray {
         int[] nums1 = {1, 1, 2};
         int[] nums2 = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
         RemoveDuplicatesFromSortedArray removeDuplicatesFromSortedArray = new RemoveDuplicatesFromSortedArray();
-        System.out.println(removeDuplicatesFromSortedArray.removeDuplicates(nums1));
-        System.out.println(removeDuplicatesFromSortedArray.removeDuplicates(nums2));
+        System.out.println(removeDuplicatesFromSortedArray.removeDuplicates2(nums1));
+        System.out.println(removeDuplicatesFromSortedArray.removeDuplicates2(nums2));
     }
 
     public int removeDuplicates(int[] nums) {
@@ -22,5 +22,17 @@ public class RemoveDuplicatesFromSortedArray {
             nums[i] = resultArray[i];
         }
         return size;
+    }
+
+    public int removeDuplicates2(int[] nums) {
+        // Since first element cant be a duplicate we initialize c as 1
+        // It represents the index which we are going to replace the next non-duplicate number;
+        int c = 1;
+        for ( int i = 1; i < nums.length; i++ ) {
+            if ( nums[i] != nums[i - 1] ) {
+                nums[c++] = nums[i];
+            }
+        }
+        return c ;
     }
 }
